f5cf19426099e05faee929c337d079eb076e6d57
Alex Ortega <alexortega@Alexs-MBP.home>
2020-03-17 10:18:23 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,55 @@
 
 
-var cards = ["king", "king", "king", 
-"king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
-var cardOne = cards[0]; 
 
-console.log("User flipped queen");
-cardsInPlay.push(cardOne);
-
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped king");
-
-
-if (cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.")
-	}
+  		console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId){ 
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+	if (cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.")
+		}
+	
+
 }
