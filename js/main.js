b3d41f9d5874b3f2c009184cee052ac0e52e11f3
Alex Ortega <alexortega@Alexs-MBP.home>
2020-03-16 21:24:48 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["king", "king", "king", 
+"king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0]; 
+
+console.log("User flipped queen");
+cardsInPlay.push(cardOne);
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
